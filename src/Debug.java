b51415bfe9e0250f63b4ad1af7c93eab16895c54
yamna-ali <yamna_fasih.92@hotmail.com>
2020-08-25 13:55:28 -0400

    first commit

---
+++ src/Debug.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Debug {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        int testscore = input.nextInt();
+        char grade;
+
+        if (testscore >= 90) {
+            grade = 'A';
+        } else if (testscore >= 80) {
+            grade = 'B';
+        } else if (testscore >= 70) {
+            grade = 'C';
+        } else if (testscore >= 60) {
+            grade = 'D';
+        } else {
+            grade = 'F';
+        }
+        System.out.println("Grade = " + grade);
+    }
+}
